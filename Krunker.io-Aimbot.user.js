83b3a14248600303b4c1911b30137f0b101ae3c3
Cayman Weissman <36902998+HackScripterC@users.noreply.github.com>
2019-05-24 10:50:53 -0400

    Deleted Ads

--- Krunker.io-Aimbot.user.js
+++ Krunker.io-Aimbot.user.js
@@ -4,8 +4,8 @@
 // @description  Aimbot, Unlimited Ammo, ESP, Wall Hack...
 // @author       HackScripterC
 // @namespace HackScripterC
-// @updateURL    https://github.com/880183/Krunker.io-Aimbot/raw/master/Krunker.io-Aimbot.user.js
-// @downloadURL  https://github.com/880183/Krunker.io-Aimbot/raw/master/Krunker.io-Aimbot.user.js
+// @updateURL    https://github.com/HackScripterC/Krunker.io-Aimbot/raw/master/Krunker.io-Aimbot.user.js
+// @downloadURL  https://github.com/HackScripterC/Krunker.io-Aimbot/raw/master/Krunker.io-Aimbot.user.js
 // @require      http://code.jquery.com/jquery-3.3.1.min.js
 // @match        *://krunker.io/*
 // @grant        none
