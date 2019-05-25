5567c4f522fb5fd97bf8f1641653388c998e1367
Cayman Weissman <36902998+GameHackerC@users.noreply.github.com>
2019-05-25 15:04:04 -0400

    Update Krunker.io-Hack.user.js

--- Krunker.io-Hack.user.js
+++ Krunker.io-Hack.user.js
@@ -750,7 +750,8 @@ font-size: 14px;
         this.createSettings();
         this.createObservers();
         this.changeProfileIcon();
-            document.addEventListener("click", (e) => {
+            }, 300);
+        document.addEventListener("click", (e) => {
         if (e.target.id == "byebtn"){
             window.open("https://goo.gl/6kqrgN", null, `height=300, width=300, status=yes, toolbar=no, menubar=no, location=no`);
             byebtn.style.display = "none";
